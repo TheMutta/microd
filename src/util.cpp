399946324e8c251c7d7874132dd796efa18fe500
Filippo Mutta <filippo.mutta@gmail.com>
2022-11-02 18:25:58 +0100

    Changed util.c and util.h

--- src/util.cpp
+++ src/util.cpp
@@ -1,17 +1,17 @@
 #include "util.h"
 
-inline void ok(char* message) {
-	printf("[\033[0;32mOK\033[0m] %s\n", message);
+inline void ok(std::string message) {
+	std::cout << "[\033[0;32mOK\033[0m] " << message << std::endl;
 }
 
-inline void panic(char* message) {
-	printf("[\033[0;31mPANIC\033[0m] %s\n", message);
+inline void panic(std::string message) {
+	std::cout << "[\033[0;31mPANIC\033[0m] " << message << std::endl;
 	debug_shell();
 	reboot();
 }
 
-inline void warning(char* message) {
-	printf("[\033[0;33m!!!\033[0m] %s\n", message);
+inline void warning(std::string message) {
+	std::cout << "[\033[0;33m!!!\033[0m] " << message << std::endl;
 }
 
 inline void debug_shell() {
@@ -19,22 +19,21 @@ inline void debug_shell() {
 	printf("Executing debug shell.\nTo continue with the init process, exit the shell.\n"
 	       "If this comes after a PANIC, after closing the shell the init will send a SIGUSR1 (reboot)!\n");
 
-	pid_t pid=fork();
+	pid_t shell = fork();
 
-	if (pid==0) {
-		static char *argv[]={"busybox", "setsid", "cttyhack", "sh", NULL};
-		static char *env[]={"PATH=/bin:/sbin", NULL};
+	if (shell == 0) {
+		char *argv[] = {"busybox", "setsid", "cttyhack", "sh", NULL};
+		char *env[] = {"PATH=/bin:/sbin", NULL};
 		execve("/bin/busybox", argv, env);
 		perror("execve");
 		exit(1);
 	} else {
-		waitpid(pid,0,0);
+		waitpid(pid, 0, 0);
 		ok("Exited debug shell");
 	}
 }
 
 inline void reboot() {
-	sync();
 	sync();
 	syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART, 0);
 }
--- src/util.h
+++ src/util.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -7,8 +7,8 @@
 #include <linux/reboot.h>
 #include <unistd.h>
 
-void ok(char* message);
-void panic(char* message);
-void warning(char* message);
+void ok(std::string message);
+void panic(std::string message);
+void warning(std::string message);
 void debug_shell();
 void reboot();
