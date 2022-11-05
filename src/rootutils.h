af3aa0a61a23c7287794b844ee13401e990c2053
Filippo Mutta <filippo.mutta@gmail.com>
2022-11-05 17:17:32 +0100

    Added separate rootinit utils.

---
+++ src/rootutils.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <sys/mount.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <iostream>
+
+#include "util.h"
+#include "tinydir.h"
+#include "mount_special.h"
+
+namespace root {
+	void remount_root_rw(char* rootfs, char* rootfs_type, unsigned long rootfs_mount_flags);
+	void mount_drive(char* drv, char* fstype, char* destdir, unsigned long flags);
+	void startup_scripts();
+	void launch_programs(); 
+}
