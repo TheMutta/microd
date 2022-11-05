121ff6aca27b0aa13d61eba4a005260a92e39819
Filippo Mutta <filippo.mutta@gmail.com>
2022-11-05 17:19:04 +0100

    Moved function to rdinit module.

--- src/rdinit.cpp
+++ src/rdinit.cpp
@@ -1,5 +1,19 @@
 #include "rdinit.h"
 
+void initrd_init(util::arguments init_arguments) {
+	util::ok("Started initrd /init");
+	
+	mount_specialfs();
+	util::ok("Finished mounting filesystems.");
+
+	if (init_arguments.is_debug) {
+		util::debug_shell();
+	}
+		
+	mount_root(init_arguments.rootdrv, init_arguments.rootfstype, MS_RDONLY | MS_NOATIME);
+	switch_root();
+}
+
 void mount_root(char* rootfs, char* rootfs_type, unsigned long rootfs_mount_flags) {
 	std::cout << "Mounting " << rootfs << " as a " << rootfs_type << " root device." << std::endl;
 	if (mount(rootfs, "/new_root", rootfs_type, rootfs_mount_flags, "") !=0 ) {
--- src/rdinit.h
+++ src/rdinit.h
@@ -2,7 +2,11 @@
 #include <sys/mount.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+
+#include "mount_special.h"
 #include "util.h"
+
+void initrd_init(util::arguments init_arguments);
 
 void mount_root(char* rootfs, char* rootfs_type, unsigned long rootfs_mount_flags);
 void switch_root();
