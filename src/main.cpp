7f7450eccd5ca654edf84bfb35b913a807c90fe0
Filippo Mutta <filippo.mutta@gmail.com>
2022-11-07 18:59:45 +0100

    Version 0.2.0-alpha completed.

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 #include "util.h"
 #include "rootutils.h"
 
-const char* version = "0.2.0-alpha-rc2";
+const char* version = "0.2.0-alpha";
 const char* author = "Mutta Filippo";
 const char* date = "2022-2022";
 
