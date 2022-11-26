6f3eeb3bf7287b67aebf2ed4a8e182825f82d60d
Filippo Mutta <filippo.mutta@gmail.com>
2022-11-26 15:25:15 +0100

    Changed const char* in #define

--- src/config.h
+++ src/config.h
@@ -1,5 +1,5 @@
 #pragma once
 
-const char* version = "0.2.0-alpha";
-const char* author = "Mutta Filippo";
-const char* date = "2022-2022";
+#define VERSION "0.2.0-alpha"
+#define AUTHOR "Mutta Filippo"
+#define DATE "2022-2022"
